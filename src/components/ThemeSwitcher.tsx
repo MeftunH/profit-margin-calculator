d2c7cbf2d949563a049e8edada4b85a7bc55bb6f
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-22 06:39:45 +0300

    Update Component contents

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,27 +1,65 @@
-import React, {useEffect, useState} from "react";
+import { useState, useEffect } from "react";
+
 function ThemeSwitcher() {
-    const [mounted, setMounted] = useState(false)
-    const [isDark, setIsDark] = useState<boolean | undefined>(undefined)
+    const [mounted, setMounted] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState<boolean | undefined>(undefined);
 
     useEffect(() => {
-        setMounted(true)
-        setIsDark(document.documentElement.classList.contains('dark'));
-    }, [])
+        setMounted(true);
+        setIsDarkMode(document.documentElement.classList.contains("dark"));
+    }, []);
+
+    useEffect(() => {
+        if (!mounted || isDarkMode === undefined) return;
+
+        if (isDarkMode) {
+            window.document.documentElement.classList.add("dark");
+            localStorage.theme = "dark";
+        } else {
+            window.document.documentElement.classList.remove("dark");
+            localStorage.theme = "light";
+        }
+    }, [isDarkMode]);
+
+    const onToggle = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
+    if (!mounted) return null;
 
     return (
         <div className="absolute top-10 right-10">
-            <button className="text-grey-500 dark:text-gray-400 hover:bg-gray-100
-                                dark:hover:bg-gray-700 focus:outline-none focus:ring-4
-                                 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                     stroke="currentColor" className="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"/>
-                </svg>
-
+            <button
+                type="button"
+                className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+                onClick={onToggle}
+            >
+                {isDarkMode ? (
+                    <svg
+                        className="w-5 h-5 "
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                        ></path>
+                    </svg>
+                ) : (
+                    <svg
+                        className="w-5 h-5 "
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                    </svg>
+                )}
             </button>
         </div>
-    )
+    );
 }
 
-export default ThemeSwitcher
+export default ThemeSwitcher;
