dd254d95776a71ef16545551d73ecdc9c054d56a
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-24 06:48:34 +0300

    Update app content

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import React from "react";
 import ThemeSwitcher from "./components/ThemeSwitcher";
+import Calculator from "./components/Calculator";
+import FAQ from "./components/FAQ";
+import ThemeColorSwitcher from "./components/ThemeColorSwitcher";
+
 function App() {
     return (
         <main className="bg-white dark:bg-black relative">
             <div className="py-24 sm:py-32">
                 <div className="mx-auto max-w-7xl px-4 lg:px-0">
-                 <ThemeSwitcher/>
-                </div>
-                <div className="mx-auto max-w-2xl sm:text-center">
-                    {/*TODO: color switch*/}
-                </div>
-                <h2 className="text-brand text-3xl font-bold tracking-tight sm:text-center">Profit Margin
-                    Calculator</h2>
-                <div className="mt-12 sm:mt-16">
-                    <div className="mx-auto max-w-2xl">
-                        <div className="bg-white dark:bg-gray-800 shadow rounded-lg px-4 py-8 sm:px-6 sm:py-10">
-                            <div className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
-                                <div>
-                                    <label htmlFor="cost"
-                                           className="block text-sm font-medium text-gray-900 dark:text-gray-100">Calculate your profit margin and see how much you can
-                                        charge for your products.</label>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    <ThemeSwitcher />
 
+                    <div className="mx-auto max-w-2xl sm:text-center">
+                        <ThemeColorSwitcher />
+                        <h2 className="text-3xl font-bold tracking-tight text-brand sm:text-4xl text-center">
+                            Profit Margin Calculator
+                        </h2>
+                        <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-white text-center">
+                            Optimize your menu item prices to maximize profits by identifying the most effective selling prices.
+                        </p>
+                    </div>
+
+                    <Calculator />
+                </div>
             </div>
+
+            <FAQ />
         </main>
-    )
+    );
 }
 
-export default App
+export default App;
